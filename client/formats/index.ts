2908d913341373d420063c6b78e49ad00d368a3c
emcrow <emcrow@sep.com>
2021-09-07 12:31:07 -0400

    Add default SVG in JSX format

--- client/formats/index.ts
+++ client/formats/index.ts
@@ -3,16 +3,19 @@ import type {Format} from '../../types'
 import fontAwesomeJSLibrary from './fontAwesomeJSLibrary.json'
 import simpleJS from './simpleJS.json'
 import simpleJSON from './simpleJSON.json'
+import svgInJSX from './svgInJSX.json'
 
 const defaultFormats: Format[] = [
     fontAwesomeJSLibrary,
     simpleJS,
     simpleJSON,
+    svgInJSX,
 ]
 
 export default defaultFormats
 export {
     fontAwesomeJSLibrary,
     simpleJS,
-    simpleJSON
+    simpleJSON,
+    svgInJSX
 }
