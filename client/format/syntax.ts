a336d6cafc9750d91d241719c0d4e70add09c810
emcrow <emcrow@sep.com>
2021-12-23 12:18:05 -0500

    WIP syntax

--- client/format/syntax.ts
+++ client/format/syntax.ts
@@ -4,24 +4,24 @@ type SyntaxContext =
     'start' |
     'icon' |
     'color' |
-    'color-nested' |
-    'color-nested-child' |
-    'color-nested-no-child'
+    'color-flat' |
+    'color-child' |
+    'color-no-child'
 
 const DOC_T = { token: "document-replacement-token", regex: new RegExp(`(?:${Object.keys(DocumentReplacementToken).join('|')})`) }
 const ICON_T = { token: "icon-replacement-token", regex: new RegExp(`(?:${Object.keys(IconReplacementToken).join('|')})`) }
 const COLOR_T = { token: "color-replacement-token", regex: new RegExp(`(?:${Object.keys(ColorReplacementToken).join('|')})`) }
-const CONTEXT_START = (context: SyntaxContext, nArgs = 0, tagname: string = context.toString()) => {
-    const args = "(?:\\s+?(.+?))?".repeat(nArgs)
-    const regex = RegExp(`\\{#${tagname}${args}\\}`)
+const CONTEXT_START = (context: SyntaxContext, { args = 0, tag = context.toString() } = {}) => {
+    const argsPattern = "(?:\\s+?(.+?))?".repeat(args)
+    const regex = RegExp(`\\{#${tag}${argsPattern}\\}`)
     return {
         token: `${context}-start`,
         regex: regex,
         next: context,
     }
 }
-const CONTEXT_END = (context: string, next: SyntaxContext, tagname: string = context.toString()) => {
-    const regex = RegExp(`\\{/${tagname}\\}`)
+const CONTEXT_END = (context: SyntaxContext, next: SyntaxContext, { tag = context.toString() } = {}) => {
+    const regex = RegExp(`\\{/${tag}\\}`)
     return {
         token: `${context}-end`,
         regex: regex,
@@ -32,8 +32,8 @@ const CONTEXT_END = (context: string, next: SyntaxContext, tagname: string = con
 const syntax: Record<SyntaxContext, unknown[]> = {
     'start': [
         DOC_T,
-        CONTEXT_START('start', 1),
-        CONTEXT_START('color', 1)
+        CONTEXT_START('start', { args: 1 }),
+        CONTEXT_START('color', { args: 1 }),
     ],
     'icon': [
         CONTEXT_END('icon', 'start'),
@@ -44,24 +44,26 @@ const syntax: Record<SyntaxContext, unknown[]> = {
         CONTEXT_END('color', 'start'),
         DOC_T,
         COLOR_T,
-        CONTEXT_START('color-nested', 1, 'nested'),
+        CONTEXT_START('color-child', { args: 1, tag: 'child' }),
+        CONTEXT_START('color-no-child', { tag: 'nochild' }),
+        CONTEXT_START('color-flat', { tag: 'flat' }),
     ],
-    'color-nested': [
-        CONTEXT_END('color-nested', 'color', 'nested'),
-        CONTEXT_START('color-nested-child', 0, 'child'),
-        CONTEXT_START('color-nested-no-child', 0, 'nochild'),
-    ],
-    'color-nested-child': [
+    'color-child': [
+        CONTEXT_END('color-child', 'color', { tag: 'child' }),
         DOC_T,
         COLOR_T,
-        { token: "color-recuse", regex: /RECURSE/ },
-        CONTEXT_END('color-nested-child', 'color-nested', 'child'),
+        { token: "color-grandchild", regex: /\{#grandchild\}/ },
     ],
-    'color-nested-no-child': [
+    'color-no-child': [
+        CONTEXT_END('color-no-child', 'color', { tag: 'nochild' }),
         DOC_T,
         COLOR_T,
-        CONTEXT_END('color-nested-no-child', 'color-nested', 'nochild'),
-    ]
+    ],
+    'color-flat': [
+        CONTEXT_END('color-flat', 'color', { tag: 'flat' }),
+        DOC_T,
+        COLOR_T,
+    ],
 }
 
 export {
